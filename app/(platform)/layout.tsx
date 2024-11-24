6695212abd44c206a29ca048d5c0af30bc91e8b9
sanskar001 <sanskarm001@gmail.com>
2024-11-24 17:42:23 +0530

    Feat: added organization control and protected route handling

--- app/(platform)/layout.tsx
+++ app/(platform)/layout.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 import { ClerkProvider } from "@clerk/nextjs";
 
 const PlatformLayour = ({ children }: { children: ReactNode }) => {
-  return <ClerkProvider>{children}</ClerkProvider>;
+  return <ClerkProvider afterSignOutUrl="/">{children}</ClerkProvider>;
 };
 
 export default PlatformLayour;
--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,5 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
 const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)", "/"]);
 
@@ -6,7 +7,26 @@ export default clerkMiddleware(async (auth, request) => {
   if (!isPublicRoute(request)) {
     await auth.protect();
   }
+
+  const { userId, orgId } = await auth();
+
+  if (userId && !orgId && request.nextUrl.pathname !== "/select-org") {
+    const url = new URL("/select-org", request.url);
+    return NextResponse.redirect(url);
+  }
+
+  if (userId && isPublicRoute(request)) {
+    let path = "/select-org";
+
+    if (orgId) {
+      path = `/organization/${orgId}`;
+    }
+    const orgSelection = new URL(path, request.url);
+    return NextResponse.redirect(orgSelection);
+  }
 });
+
+// export default clerkMiddleware();
 
 export const config = {
   matcher: [
