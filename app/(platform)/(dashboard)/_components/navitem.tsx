a724477ced94d47d91b8e74baa5ae6081d780a2d
sanskar001 <sanskarm001@gmail.com>
2024-11-28 23:03:36 +0530

    Feat: added sidebar skeleton

--- app/(platform)/(dashboard)/_components/navitem.tsx
+++ app/(platform)/(dashboard)/_components/navitem.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { FC } from "react";
 import Image from "next/image";
 import { cn } from "@/lib/utils";
 import {
@@ -11,6 +10,7 @@ import {
 import { Activity, CreditCard, Layout, Settings } from "lucide-react";
 import { useRouter, usePathname } from "next/navigation";
 import { Button } from "@/components/ui/button";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export type Organization = {
   id: string;
@@ -26,12 +26,12 @@ export interface NavItemProps {
   onExpand: (id: string) => void;
 }
 
-const NavItem: FC<NavItemProps> = ({
+const NavItem = ({
   isActive,
   isExpanded,
   organization,
   onExpand,
-}) => {
+}: NavItemProps) => {
   const router = useRouter();
   const pathname = usePathname();
 
@@ -101,6 +101,17 @@ const NavItem: FC<NavItemProps> = ({
         ))}
       </AccordionContent>
     </AccordionItem>
+  );
+};
+
+NavItem.Skeleton = function SkeletonNavItem() {
+  return (
+    <div className="flex items-center gap-x-2">
+      <div className="w-10 h-10 relative shrink-0">
+        <Skeleton className="h-full w-full absolute" />
+      </div>
+      <Skeleton className="h-10 w-full" />
+    </div>
   );
 };
 
